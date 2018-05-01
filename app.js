a0239b2df87d21c6dee090d96b033c29fd7d4d63
Julen Rojo <julenr@Julens-MacBook-Pro.local>
2018-05-01 17:26:32 +1200

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const koa = require('koa');
+const convert = require('koa-convert');
+const bodyParser = require('koa-bodyparser');
+const router = require('koa-simple-router');
+const error = require('koa-json-error');
+const logger = require('koa-logger');
+const koaRes = require('koa-res');
+const handleError = require('koa-handle-error');
+const serve = require('koa-static');
+const mount = require('koa-mount');
+// const WebSocket = require('ws');
+
+// const task = require('./controller/task');
+// const videoAnalisys = require('./controller/video-analisys');
+
+// const wss = new WebSocket.Server({ port: 8080 });
+const app = new koa();
+
+// error handling
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit('error', err, ctx);
+  }
+});
+
+app.use(serve('./public'));
+// logging
+app.use(logger());
+// body parsing
+app.use(bodyParser({ jsonLimit: '50mb' }));
+// format response as JSON
+app.use(convert(koaRes()));
+// configure router
+app.use(
+  router(_ => {
+    _.get('/api/v1/dummy/dummyfile.json', async ctx => {
+      ctx.body = 'hello world';
+    }),
+      _.get('/throwerror', async ctx => {
+        throw new Error('Aghh! An error!');
+      });
+  })
+);
+
+// wss.on('connection', ws => {
+//   console.log('WS Connected on port: 8080');
+//   ws.on('message', function incoming(message) {
+//     console.log('received: %s', message);
+//   });
+// });
+
+var config = {
+  domain: 'biodetection.com',
+  http: {
+    port: 8989,
+  },
+  https: {
+    port: 443,
+    options: {
+      key: fs
+        .readFileSync(path.resolve(process.cwd(), 'certs/server.key'), 'utf8')
+        .toString(),
+      cert: fs
+        .readFileSync(path.resolve(process.cwd(), 'certs/server.crt'), 'utf8')
+        .toString(),
+    },
+  },
+};
+
+// try {
+//   const httpsServer = https.createServer(config.https.options, app.callback());
+//   httpsServer.listen(config.https.port, function(err) {
+//     if (!!err) {
+//       console.error('HTTPS server FAIL: ', err, err && err.stack);
+//     } else {
+//       console.log(
+//         `HTTPS server OK: http://${config.domain}:${config.https.port}`
+//       );
+//     }
+//   });
+// } catch (ex) {
+//   console.error('Failed to start HTTPS server\n', ex, ex && ex.stack);
+// }
+
+app.listen(8080, () => {
+  console.log('API listening on port 8080');
+});
